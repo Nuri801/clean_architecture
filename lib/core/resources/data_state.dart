cd9a1309d0d007a41c159a86e337d5dbc62577a0
nuri <nuriddin@utec.kr>
2023-08-11 15:02:02 +0900

    "Models file added."

--- lib/core/resources/data_state.dart
+++ lib/core/resources/data_state.dart
@@ -6,11 +6,9 @@ abstract class DataState<T> {
 
   const DataState({this.data, this.error});
 }
-
 class DataSuccess<T> extends DataState<T> {
   const DataSuccess(T data) : super(data: data);
 }
-
 class DataFailed<T> extends DataState<T> {
   const DataFailed(DioError error) : super(error: error);
 }
